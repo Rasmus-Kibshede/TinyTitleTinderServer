b7408b45e5283300c553ca2b02a10b117fa19fdd
Roed <Geofery10@hotmail.com>
2023-09-23 23:37:55 +0200

    removed eslint from backend

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,8 +1,3 @@
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable linebreak-style */
-/* eslint-disable @typescript-eslint/object-curly-spacing */
-/* eslint-disable linebreak-style */
-/* eslint-disable new-cap */
 import { Router } from 'express';
 import { User } from '../model/user';
 const router = Router();
@@ -16,7 +11,6 @@ router.get('/user/:id', (req, res) => {
 		createdAt: '1-12-22 12:00',
 		lastLogin: '2-12-22 13:00',
 	};  
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 	res.send({data: user});
 });
 
