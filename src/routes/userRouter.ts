6651628be467070532273c428f8af6eedeade43e
Rasmus Kibshede <2740ralle@gmail.com>
2023-09-25 13:13:27 +0200

    reworked: userRouter and index and connection

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,13 +1,20 @@
-
-import {Router} from 'express';
-// Import { User } from '../model/user'
+import { Router } from 'express';
+// import { User } from '../model/user';
 import connection from '../database/connectionMYSQL';
+import { RowDataPacket } from 'mysql2';
 
 const router = Router();
 
 router.get('/user/:id', async (req, res) => {
-	const [rows] = await connection.query('select * from user where user_id = (?)', [req.params.id]);
-	res.send({data: rows});
+	if (!req.params.id) {
+		return res.status(400).send({ error: 'User id is required' });
+	} else {
+		const [rows] = await connection.query<RowDataPacket[]>('SELECT * FROM user WHERE user_id = (?)', [req.params.id]);
+
+		if (rows.length === 0)
+			return res.status(404).send({ error: 'User not found' });
+		return res.send({ data: rows[0] });
+	}
 });
 
 export default router;
