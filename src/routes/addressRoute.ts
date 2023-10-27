013a36927c65415440d7d6db046f09c5a8df2392
Roed <Geofery10@hotmail.com>
2023-10-27 12:44:41 +0200

    fixed access to endpoints Adresses

--- src/routes/addressRoute.ts
+++ src/routes/addressRoute.ts
@@ -1,9 +1,10 @@
-import Router from 'express'; 
+import { Router } from 'express';
 import { validateCredintials } from '../Utils/routeUtil';
 import { createAddress, getAllAddresses } from '../Controllers/addressController';
 
-const addressRouter = Router(); 
+const addressRouter = Router();
 
 addressRouter.post('/addresses', validateCredintials, createAddress);
 addressRouter.get('/addresses', getAllAddresses);
 
+export default addressRouter;
