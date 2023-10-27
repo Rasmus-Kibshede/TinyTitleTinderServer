38f4bbfcea34f2b3962ff94d58d5bf0d38cabd66
Joachim Tyrfing Richter <joac0724@stud.kea.dk>
2023-10-27 11:19:00 +0200

    added: name entity

---
+++ src/Entities/Name.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Name {
+
+    @PrimaryGeneratedColumn({ name: 'name_suggest_id' })
+    nameSuggestId: number;
+
+    @Column('varchar', { length: 255, nullable: false, name: 'name_suggest_name' })
+    nameSuggestName: string;
+
+    @Column('varchar', { length: 255, nullable: false, name: 'gender' })
+    gender: string;
+
+    @Column('int', { nullable: true, name: 'popularity' })
+    popularity: number | null;
+
+    @Column('datetime', { nullable: true, name: 'name_days' })
+    nameDays: Date | null;
+
+    @Column('varchar', { length: 255, nullable: true, name: 'namesakes' })
+    namesakes: string | null;
+
+}
