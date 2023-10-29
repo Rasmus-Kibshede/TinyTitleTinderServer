c5f92a9ea24c3200c8fb1b9ffd0d38c0f7904724
Roed <Geofery10@hotmail.com>
2023-10-29 21:29:55 +0100

    Crud Address complete

--- src/routes/addressRoute.ts
+++ src/routes/addressRoute.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import { createAddress, getAllAddresses, getAddressById, updateAddress, deleteAdress } from '../Controllers/addressController';
+import * as addressController from '../Controllers/addressController';
+import { validateParamsId } from '../Utils/routeUtil';
 
 const addressRouter = Router();
 
-addressRouter.post('/addresses', createAddress);
-addressRouter.get('/addresses', getAllAddresses);
-addressRouter.get('/addresses/:id', getAddressById);
-addressRouter.put('/addresses', updateAddress);
-addressRouter.put('/addresses/:id', deleteAdress);
+addressRouter.post('/addresses', addressController.createAddress);
+addressRouter.get('/addresses', addressController.getAllAddresses);
+addressRouter.get('/addresses/:id', validateParamsId, addressController.getAddressById);
+addressRouter.put('/addresses', addressController.updateAddress);
+addressRouter.put('/addresses/:id', validateParamsId, addressController.deleteAdress);
 
 export default addressRouter;
