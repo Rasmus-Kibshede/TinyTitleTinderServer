9dcece4206e988267311396e00f564acc766e73b
Roed <Geofery10@hotmail.com>
2023-11-07 12:59:16 +0100

    fixed comment

--- src/Controllers/responseController.ts
+++ src/Controllers/responseController.ts
@@ -2,6 +2,6 @@ import { BaseError } from '../Utils/BaseError';
 import { Result, ApiResponse } from '../Utils/errorHandler';
 import { Response } from 'express';
 
-export function response(res: Response<unknown, Record<string, unknown>>, response: Result<ApiResponse, BaseError>, statusCodeSuccess: number) {
+export const response = (res: Response<unknown, Record<string, unknown>>, response: Result<ApiResponse, BaseError>, statusCodeSuccess: number) => {
     res.status(response.success ? statusCodeSuccess : Number(response.error.statusCode)).send(response.success ? { success: response.success, data: response.result.data } : { success: response.success, message: response.error.message });
-}
+};
