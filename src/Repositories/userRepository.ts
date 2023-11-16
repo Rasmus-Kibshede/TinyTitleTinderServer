27c5a22efa0f1204ba6d28eb5945e873ce4d00a6
Roed <Geofery10@hotmail.com>
2023-11-16 10:50:39 +0100

    finished getting list of names,origin and meaning from parentID, stored procedure

--- src/Repositories/userRepository.ts
+++ src/Repositories/userRepository.ts
@@ -45,18 +45,7 @@ export const userRepo = appDataSource.getRepository(User).extend({
     },
     //stored procedure
     signUp(params: unknown[]) { //userRequestDTO: UserRequestDTO
-        return userRepo.query('call CreateUserWithRoleAndLocation(?,?,?,?,?,?,?,?,?,?)',params /*[
-            userRequestDTO.email,
-            userRequestDTO.password,
-            userRequestDTO.parent?.age,
-            userRequestDTO.parent?.gender,
-            userRequestDTO.parent?.firstName,
-            userRequestDTO.parent?.lastName,
-            userRequestDTO.parent?.address.location?.locationId,
-            userRequestDTO.parent?.address.city,
-            userRequestDTO.parent?.address.zipcode,
-            userRequestDTO.parent?.address.address
-        ]*/);
+        return userRepo.query('call CreateUserWithRoleAndLocation(?,?,?,?,?,?,?,?,?,?)',params);
     }
 });
 
--- src/routes/nameRoute.ts
+++ src/routes/nameRoute.ts
@@ -7,6 +7,7 @@ const nameRouter = Router();
 nameRouter.post('/names', validateDate, nameController.createName);
 nameRouter.get('/names/:id', validateParamsId, nameController.getNameByID);
 nameRouter.get('/names', nameController.getAllNames);
+nameRouter.get('/names/parent/:id', nameController.getNamesByParentId);
 nameRouter.put('/names', validateDate, nameController.updateName);
 nameRouter.put('/names/:id', validateParamsId, nameController.deleteNameByID);
 
