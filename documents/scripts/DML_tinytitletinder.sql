99a5fbc10436231c0a466f2ae2ec73c52c1a63fc
Roed <Geofery10@hotmail.com>
2023-11-23 10:56:52 +0100

    fixed dml script

--- documents/scripts/DML_tinytitletinder.sql
+++ documents/scripts/DML_tinytitletinder.sql
@@ -1,10 +1,10 @@
 USE tiny_title_tinder_database;
 
-INSERT INTO origin (region, religion) VALUES
-('United States', 'Christianity'),
-('India', 'Hinduism'),
-('Brazil', 'Catholicism'),
-('China', 'Buddhism');
+INSERT INTO origin (region, religion, description) VALUES
+('United States', 'Christianity', 'The land of the free and school shootings'),
+('India', 'Hinduism', 'Budda checkin'),
+('Brazil', 'Catholicism', 'Pendejo'),
+('China', 'Buddhism', 'The religion of China should be Taoism, not buddhism. Taiwan # 1');
 
 INSERT INTO definition (meaning) VALUES
 ('The name "John" is of male gender and has a popularity score of 0.85. It is associated with the nameday of June 24 and has notable namesakes such as John F. Kennedy. It also has related names like Jonathan and Jack.'),
@@ -13,26 +13,25 @@ INSERT INTO definition (meaning) VALUES
 ('The name "Wei" is of male gender and has a popularity score of 0.67. It is associated with the nameday of September 12 and has notable namesakes such as Wei Jingsheng. It also has related names like Wey and Wayne.');
 
 INSERT INTO name_suggest (name_suggest_name, gender, popularity, name_days, namesakes) VALUES
-('John', 'Male', 0.85, '2000-01-09', 'John F. Kennedy'),
-('Priya', 'Female', 0.92, '2000-02-08', 'Priyanka Chopra'),
-('Carlos', 'Male', 0.78, '2000-03-07', 'Carlos Santana'),
-('Wei', 'Male', 0.67, '2000-04-06', 'Wei Jingsheng'),
-('Hans', 'Male', 0.31, '2000-05-05', 'Hans Pilgaard'),
-('Hansi', 'Male', 0.31, '2000-06-04', 'Hans Pilgaard'),
-('Margot', 'Female', 0.14, '2000-07-03', 'Margot Robbie'),
-('Maria', 'Female', 0.73, '2000-08-02', 'Maria Bello');
-
-INSERT INTO name_suggest_definition (fk_name_suggest_id, fk_definition_id) VALUES
-(1, 2),
-(2, 3),
-(3, 4),
-(4, 1);
+('John', 'Male', 0.85, '01-01-2023', 'John F. Kennedy'),
+('Priya', 'Female', 0.92, '01-01-2023', 'Priyanka Chopra'),
+('Carlos', 'Male', 0.78, '01-01-2023', 'Carlos Santana'),
+('Wei', 'Male', 0.67, '01-01-2023', 'Wei Jingsheng'),
+('Hans', 'Male', 0.31, '01-01-2023', 'Hans Pilgaard'),
+('Hansi', 'Male', 0.31, '01-01-2023', 'Hans Pilgaard'),
+('Margot', 'Female', 0.14, '01-01-2023', 'Margot Robbie'),
+('Maria', 'Female', 0.73, '01-01-2023', 'Maria Bello');
 
 INSERT INTO user (email, password, user_active, created_at, last_login) VALUES
-('user1@example.com', 'password1', 1, '2012-11-05 14:29:36', '2012-12-05 14:29:36'),
-('user2@example.com', 'password2', 1, '2016-11-05 14:29:36', '2016-12-05 14:29:36'),
-('user3@example.com', 'password3', 1, '2012-11-05 14:29:36', '2012-12-05 14:29:36'),
-('user4@example.com', 'password4', 1, '2019-11-05 14:29:36', '2019-12-05 14:29:36');
+('user1@example.com', 'password1', 1, '2012-11-05 14:29:36', NOW() - INTERVAL 1 YEAR),
+('user2@example.com', 'password2', 1, '2016-11-05 14:29:36', NOW() - INTERVAL 1 MONTH),
+('user3@example.com', 'password3', 1, '2012-11-05 14:29:36', NOW() - INTERVAL 1 WEEK),
+('user4@example.com', 'password4', 1, '2019-11-05 14:29:36', NOW() - INTERVAL 1 DAY);
+
+INSERT INTO role (title) VALUES
+('Super Admin'),
+('Admin'),
+('User');
 
 INSERT INTO user_role (fk_user_id, fk_role_id) VALUES
 (1, 1),
@@ -46,11 +45,11 @@ INSERT INTO address (city, zipcode, address) VALUES
 ('Rio de Janeiro', '20000', '789 Oak St'),
 ('Beijing', '100000', '101 Pine St');
 
-INSERT INTO location (country, fk_address_id) VALUES
-('United States', 1),
-('India', 2),
-('Brazil', 3),
-('China', 4);
+INSERT INTO location (country) VALUES
+('United States'),
+('India'),
+('Brazil'),
+('China');
 
 INSERT INTO family (family_name) VALUES
 ('Smith'),
@@ -58,22 +57,11 @@ INSERT INTO family (family_name) VALUES
 ('Silva'),
 ('Wang');
 
-INSERT INTO invite (invite_status, fk_family_id) VALUES
-(1, 1),
-(0, 2),
-(1, 3),
-(0, 4);
-
-INSERT INTO parent (age, gender, first_name, last_name, fk_user_id, fk_location_id, fk_invite_id) VALUES
-(35, 'Male', 'John', 'Smith', 1, 1, 1),
-(30, 'Female', 'Priya', 'Patel', 2, 2, 2),
-(40, 'Male', 'Carlos', 'Silva', 3, 3, 3),
-(32, 'Male', 'Wei', 'Wang', 4, 4, 4);
-
-INSERT INTO role (role) VALUES
-('Super Admin'),
-('Admin'),
-('User');
+INSERT INTO parent (age, gender, first_name, last_name) VALUES
+(35, 'Male', 'John', 'Smith'),
+(30, 'Female', 'Priya', 'Patel'),
+(40, 'Male', 'Carlos', 'Silva'),
+(32, 'Male', 'Wei', 'Wang');
 
 INSERT INTO name_suggest_origin (fk_name_suggest_id, fk_origin_id) VALUES
 (1, 1),
