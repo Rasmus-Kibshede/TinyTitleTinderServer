8bd3ba5882b5512a00d1c086c317959305c68ddf
Roed <Geofery10@hotmail.com>
2023-11-27 22:12:35 +0100

    data

--- src/Repositories/data-source.ts
+++ src/Repositories/data-source.ts
@@ -2,18 +2,7 @@ import { DataSource } from 'typeorm';
 import { AuditingSubscriber } from 'typeorm-auditing';
 
 export const appDataSource = new DataSource({
-<<<<<<< HEAD
-  type: 'mysql',
-  host: process.env.L_HOST,
-  username: process.env.L_USERNAME,
-  database: process.env.L_DATABASE,
-  password: process.env.L_PASSWORD,
-  port: Number(process.env.L_PORT),
-  entities: ['src/Entities/**/*.ts'],
-  subscribers: [AuditingSubscriber],
-  synchronize: process.env.SYNCHRONIZE === 'true' || false,
-  logging: false,
-=======
+
     type: 'mysql',
     host: process.env.DB_HOST,
     username: process.env.DB_USERNAME,
@@ -22,7 +11,6 @@ export const appDataSource = new DataSource({
     port: Number(process.env.DB_PORT),
     entities: ['src/Entities/**/*.js'],
     subscribers: [AuditingSubscriber],
-    synchronize: false,
-    logging: false
->>>>>>> 29bf62c82fc5f63f4d17533fa384d9b5f310ec89
+    synchronize: process.env.SYNCHRONIZE === 'true' || false,
+    logging: false,
 });
