9402a46983a6cae263e85d5a092adc1ee0853c05
Joachim Tyrfing Richter <joac0724@stud.kea.dk>
2023-12-04 14:37:08 +0100

    added: adt foreign key table for user / role relationship

--- src/Entities/AuditEntities/RoleADT.ts
+++ src/Entities/AuditEntities/RoleADT.ts
@@ -1,9 +1,15 @@
 import { AuditingAction, AuditingEntity, AuditingEntityDefaultColumns } from 'typeorm-auditing';
 import { Role } from '../Role';
+import { UserADT } from './UserADT';
+import { ManyToMany, JoinColumn } from 'typeorm';
 
 @AuditingEntity(Role, { name: 'adt_role' })
 export class RoleADT extends Role implements AuditingEntityDefaultColumns {
     readonly _seq!: number;
     readonly _action!: AuditingAction.Update;
     readonly _modifiedAt!: Date;
+
+    @ManyToMany(()=> UserADT, (user) => user.roles)
+    @JoinColumn()
+    users: UserADT[];
 }
--- src/Entities/AuditEntities/UserADT.ts
+++ src/Entities/AuditEntities/UserADT.ts
@@ -1,14 +1,30 @@
 // TODO: Update modifiedBy column to log user who made changes
 import { AuditingAction, AuditingEntity, AuditingEntityDefaultColumns } from 'typeorm-auditing';
 import { User } from '../User';
-import { Column, BeforeInsert } from 'typeorm';
+import { Column, BeforeInsert, JoinColumn, JoinTable, ManyToMany } from 'typeorm';
 import { appDataSource } from '../../Repositories/data-source';
+import { RoleADT } from './RoleADT';
 
 @AuditingEntity(User, { name: 'adt_user' })
 export class UserADT extends User implements AuditingEntityDefaultColumns {
     readonly _seq!: number;
     readonly _action!: AuditingAction;
     readonly _modifiedAt!: Date;
+
+    @ManyToMany(() => RoleADT, (role) => role.users, { nullable: true })
+    @JoinColumn()
+    @JoinTable({
+        name: 'adt_user_role',
+        joinColumn: {
+            name: 'fk_user_id',
+            referencedColumnName: 'userId'
+        },
+        inverseJoinColumn: {
+            name: 'fk_role_id',
+            referencedColumnName: 'roleId'
+        }
+    })
+    roles: RoleADT[] | null;
 
     @BeforeInsert()
     dropFkParentId() {
