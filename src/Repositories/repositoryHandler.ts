c2405a68b3c79f91eb1e4d4d937386e54f7fa4a8
Roed <Geofery10@hotmail.com>
2023-12-04 14:52:23 +0100

    refractor

--- src/Repositories/repositoryHandler.ts
+++ src/Repositories/repositoryHandler.ts
@@ -4,11 +4,13 @@ import { parentRepo } from './mysql/parentRepository';
 import { parentRepo as mongoParent } from './mysql/parentRepository';
 
 export const data = (entity: string) => {
-const repos = [{name: 'mysqlUser', repo: mysql}, {name: 'mongoUser', repo:  mongo}, {name: 'mysqlParent', repo: parentRepo}, {name: 'mongoParent', repo: mongoParent}];
-  switch (global.dbChoice) {
+  const dbChoice = global.dbChoice;
+const repos = [{name: `${dbChoice}User`, repo: mysql}, {name: `${dbChoice}User`, repo:  mongo}, {name: `${dbChoice}Parent`, repo: parentRepo}, 
+{name: `${dbChoice}Parent`, repo: mongoParent}];
+  switch (dbChoice) {
     case 'mysql':
-      return repos.find((repo)=> repo.name === entity)?.repo;
+      return repos.find((repo)=> repo.name === dbChoice + entity)?.repo;
     case 'mongodb':
-      return repos.find((repo)=> repo.name === entity)?.repo;
+      return repos.find((repo)=> repo.name === dbChoice + entity)?.repo;
   }
 };
